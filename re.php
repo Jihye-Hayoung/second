e2682aaddf42e73cafd34906261b150d0a00ea6f
hayoung <kimhayoung0311@gmail.com>
2018-04-17 22:06:02 +0900

    php file upload 1
    -configure
    -connection

---
+++ re.php
@@ -0,0 +1,9 @@
+<?php
+
+$conn = mysqli(localhost,'root','root','hufsgo');
+
+$sql="INSERT into user(name,email, password) values(_$post('InputName'),)"
+
+
+
+ ?>
---
+++ welcome.php
@@ -0,0 +1,15 @@
+<?php
+include ('check.php');
+ ?>
+
+ <html>
+ <head>
+   <title> WELCOME </title>
+ </head>
+
+ <body>
+
+<a href="/index.html">  //로그인 성공후 이동할 페이지 -> 메인 페이지 (index.html)
+
+ </body>
+</html>
